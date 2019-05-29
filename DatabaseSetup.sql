9a84c82875512fb5fe642907b8cd6aad5977a38a
Kyler Griffith <lockheedmartian@gmail.com>
2019-05-29 11:46:07 -0700

    update

--- DatabaseSetup.sql
+++ DatabaseSetup.sql
@@ -3,38 +3,39 @@ USE Group6_Spotify
 
 -- I haven't run this code yet, please don't run this code yet --
 CREATE TABLE [tblDEVICE_TYPE] (
-  [deviceTypeID] INT,
-  [deviceTypeName] varchar(50),
-  [deviceTypeDescr] varchar(250)
+  [deviceTypeID] <type>,
+  [deviceTypeName] <type>,
+  [deviceTypeDescr] <type>
 );
 
 CREATE INDEX [pk] ON  [tblDEVICE_TYPE] ([deviceTypeID]);
 
 CREATE TABLE [tblRECORDING] (
-  [recordingID] INT,
-  [songID] INT,
-  [genreID] INT,
-  [recordingLength] numeric(100, 2)
+	  [recordingID] <type>,
+	  [songGroupID] <type>,
+	  [genreID] <type>,
+	  [recordingRatingID] <type>,
+	  [recordingLength] <type>
 );
 
 CREATE INDEX [pk] ON  [tblRECORDING] ([recordingID]);
 
-CREATE INDEX [fk] ON  [tblRECORDING] ([songID], [genreID]);
+CREATE INDEX [fk] ON  [tblRECORDING] ([songGroupID], [genreID], [recordingRatingID]);
 
 CREATE TABLE [tblARTIST] (
-  [artistID] INT,
-  [artistFName] varchar(50),
-  [artistLName] varchar(50),
-  [artistBio] varchar(500),
-  [artistPic] varchar(100)
+	  [artistID] <type>,
+	  [artistFName] <type>,
+	  [artistLName] <type>,
+	  [artistBio] <type>,
+	  [artistPic] <type>
 );
 
 CREATE INDEX [pk] ON  [tblARTIST] ([artistID]);
 
 CREATE TABLE [tblCUSTOMER_DEVICE] (
-  [custDeviceID] INT,
-  [custID] INT,
-  [deviceID] INT
+	  [custDeviceID] <type>,
+	  [custID] <type>,
+	  [deviceID] <type>
 );
 
 CREATE INDEX [pk] ON  [tblCUSTOMER_DEVICE] ([custDeviceID]);
@@ -42,34 +43,43 @@ CREATE INDEX [pk] ON  [tblCUSTOMER_DEVICE] ([custDeviceID]);
 CREATE INDEX [fk] ON  [tblCUSTOMER_DEVICE] ([custID], [deviceID]);
 
 CREATE TABLE [tblCUSTOMER] (
-  [custID] INT,
-  [custTypeID] INT,
-  [custFName] varchar(50),
-  [custLName] varchar(50),
-  [custDOB] Date,
-  [custAddress] varchar(75),
-  [custState] varchar(5),
-  [custZip] varchar(50),
-  [custLanguage] varchar(50),
-  [custPic] varchar(100)
+	  [custID] <type>,
+	  [custTypeID] <type>,
+	  [custFName] <type>,
+	  [custLName] <type>,
+	  [custDOB] <type>,
+	  [custAddress] <type>,
+	  [custState] <type>,
+	  [custZip] <type>,
+	  [custLanguage] <type>,
+	  [custPic] <type>
 );
 
 CREATE INDEX [pk] ON  [tblCUSTOMER] ([custID]);
 
 CREATE INDEX [fk] ON  [tblCUSTOMER] ([custTypeID]);
 
+CREATE TABLE [tbl_GROUP] (
+	  [groupID] <type>,
+	  [groupName] <type>,
+	  [groupBio] <type>,
+	  [groupPic] <type>
+);
+
+CREATE INDEX [pk] ON  [tbl_GROUP] ([groupID]);
+
 CREATE TABLE [tblCUSTOMER_TYPE] (
-  [custTypeID] INT,
-  [custTypeName] varchar(50),
-  [custTypeDecr] varchar(250)
+	  [custTypeID] <type>,
+	  [custTypeName] <type>,
+	  [custTypeDescr] <type>
 );
 
 CREATE INDEX [pk] ON  [tblCUSTOMER_TYPE] ([custTypeID]);
 
 CREATE TABLE [tblDEVICE] (
-  [deviceID] INT,
-  [deviceTypeID] INT,
-  [deviceName] varchar(50)
+	  [deviceID] <type>,
+	  [deviceTypeID] <type>,
+	  [deviceName] <type>
 );
 
 CREATE INDEX [pk] ON  [tblDEVICE] ([deviceID]);
@@ -77,75 +87,121 @@ CREATE INDEX [pk] ON  [tblDEVICE] ([deviceID]);
 CREATE INDEX [fk] ON  [tblDEVICE] ([deviceTypeID]);
 
 CREATE TABLE [tblROLE] (
-  [roleID] INT,
-  [roleName] varchar(50),
-  [roleDescr] varchar(250)
+	  [roleID] <type>,
+	  [roleName] <type>,
+	  [roleDescr] <type>
 );
 
 CREATE INDEX [pk] ON  [tblROLE] ([roleID]);
 
-CREATE TABLE [tblSONG_ARTIST_ROLE] (
-  [songArtistRoleID] INT,
-  [artistID] INT,
-  [roleID] INT,
-  [songID] INT
+CREATE TABLE [tblGROUP_MEMBER] (
+	  [groupMemberID] <type>,
+	  [groupID] <type>,
+	  [artistID] <type>,
+	  [roleID] <type>
 );
 
-CREATE INDEX [pk] ON  [tblSONG_ARTIST_ROLE] ([songArtistRoleID]);
+CREATE INDEX [pk] ON  [tblGROUP_MEMBER] ([groupMemberID]);
 
-CREATE INDEX [fk] ON  [tblSONG_ARTIST_ROLE] ([artistID], [roleID], [songID]);
+CREATE INDEX [fk] ON  [tblGROUP_MEMBER] ([groupID], [artistID], [roleID]);
+
+CREATE TABLE [tblRECORDING_RATING] (
+	  [recordingRatingID] <type>,
+	  [ratingName] <type>
+);
+
+CREATE INDEX [pk] ON  [tblRECORDING_RATING] ([recordingRatingID]);
 
 CREATE TABLE [tblALBUM] (
-  [albumID] INT,
-  [albumName] varchar(50),
-  [albumPic] varchar(100),
-  [albumDescr] varchar(250)
+	  [albumID] <type>,
+	  [albumName] <type>,
+	  [albumPic] <type>,
+	  [albumDescr] <type>,
+	  [albumLabel] <type>,
+	  [albumReleaseDate] <type>
 );
 
 CREATE INDEX [pk] ON  [tblALBUM] ([albumID]);
 
+CREATE TABLE [tblPLAYLIST] (
+	  [playlistID] <type>,
+	  [playlistTypeID] <type>,
+	  [playlistName] <type>,
+	  [playlistDescr] <type>
+);
+
+CREATE INDEX [pk] ON  [tblPLAYLIST] ([playlistID]);
+
+CREATE INDEX [fk] ON  [tblPLAYLIST] ([playlistTypeID]);
+
+CREATE TABLE [tblPLAYLIST_TYPE] (
+	  [playlistTypeID] <type>,
+	  [playlistTypeName] <type>
+);
+
+CREATE INDEX [pk] ON  [tblPLAYLIST_TYPE] ([playlistTypeID]);
+
+CREATE TABLE [tblCUSTOMER_PLAYLIST] (
+	  [customerPlaylistID] <type>,
+	  [custID] <type>,
+	  [playlistID] <type>
+);
+
+CREATE INDEX [pk] ON  [tblCUSTOMER_PLAYLIST] ([customerPlaylistID]);
+
+CREATE INDEX [fk] ON  [tblCUSTOMER_PLAYLIST] ([custID], [playlistID]);
+
 CREATE TABLE [tblEVENT] (
-  [eventID] INT,
-  [custID] INT,
-  [recordingID] INT,
-  [eventTypeID] INT,
-  [eventDate] Date
+	  [eventID] <type>,
+	  [recordingID] <type>,
+	  [eventTypeID] <type>,
+	  [custID] <type>,
+	  [eventDate] <type>
 );
 
 CREATE INDEX [pk] ON  [tblEVENT] ([eventID]);
 
-CREATE INDEX [fk] ON  [tblEVENT] ([custID], [recordingID], [eventTypeID]);
+CREATE INDEX [fk] ON  [tblEVENT] ([recordingID], [eventTypeID], [custID]);
 
 CREATE TABLE [tblRECORDING_ALBUM] (
-  [recordingAlbumID] INT,
-  [recordingID] INT,
-  [albumID] INT
+	  [recordingAlbumID] <type>,
+	  [recordingID] <type>,
+	  [albumID] <type>
 );
 
 CREATE INDEX [pk] ON  [tblRECORDING_ALBUM] ([recordingAlbumID]);
 
 CREATE INDEX [fk] ON  [tblRECORDING_ALBUM] ([recordingID], [albumID]);
 
+CREATE TABLE [tblSONG_GROUP] (
+	  [songGroupID] <type>,
+	  [songID] <type>,
+	  [groupID] <type>
+);
+
+CREATE INDEX [pk] ON  [tblSONG_GROUP] ([songGroupID]);
+
+CREATE INDEX [fk] ON  [tblSONG_GROUP] ([songID], [groupID]);
+
 CREATE TABLE [tblEVENT_TYPE] (
-  [eventTypeID] INT,
-  [eventTypeName] varchar(50),
-  [eventTypeDecr] varchar(250)
+	  [eventTypeID] <type>,
+	  [eventTypeName] <type>,
+	  [eventTypeDescr] <type>
 );
 
 CREATE INDEX [pk] ON  [tblEVENT_TYPE] ([eventTypeID]);
 
 CREATE TABLE [tblGENRE] (
-  [genreID] INT,
-  [genreName] varchar(50),
-  [genreDescr] varchar(250)
+	  [genreID] <type>,
+	  [genreName] <type>,
+	  [genreDescr] <type>
 );
 
 CREATE INDEX [pk] ON  [tblGENRE] ([genreID]);
 
 CREATE TABLE [tblSONG] (
-  [songID] INT,
-  [songName] varchar(50)
+	  [songID] <type>,
+	  [songName] <type>
 );
 
 CREATE INDEX [pk] ON  [tblSONG] ([songID]);
-

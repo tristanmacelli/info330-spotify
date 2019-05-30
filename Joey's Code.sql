ecfd61a6bf7fa4ea1569d86ab084dc0179e865cb
Joey Ravner <jravner@uw.edu>
2019-05-30 13:43:12 -0700

    add Joeys SQL file

---
+++ Joey's Code.sql
@@ -0,0 +1,25 @@
+-- Joey's SQL Code
+
+-- Transactional Table Stored Procedures
+-- tblEVENT, tblCUSTOMER_DEVICE
+
+
+-- Look-Up Table Stored Procedures
+-- tblSONG, tblGROUP
+
+
+--Business Rule
+/* DESCRIPTION:
+Can’t create a private playlist with a free account type.
+
+*/
+-- Code:
+
+
+-- Computed Column
+-- Time listened / user
+
+
+-- Query
+-- Write the SQL query to list the top 3 artist who has the most listened 
+    -- between 2012 and 2015, in Jazz.
